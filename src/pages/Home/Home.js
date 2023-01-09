cff7bc74d0476a9629c8c915da32f55d848fd248
tunny17 <tvnji17@gmail.com>
2023-01-10 00:45:28 +0100

    addition of the navbar component in the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
 
 const Home = () => {
   return (
     <div>
-      home
+      <Navbar />
     </div>
   )
 }
